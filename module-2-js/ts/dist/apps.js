ff5afa550b542c7ebe54884638ee4fa5d63bee40
amit reuveni <amitr123r@gmail.com>
2025-01-07 22:34:30 +0200

    first.code, tall.enough.to.ride & calculator

--- module-2-js/ts/dist/apps.js
+++
@@ -1,9 +0,0 @@
-// 1
-;
-var firstNumber = Number(prompt("enter first number"));
-var secondNumber = Number(prompt("enter second number"));
-alert(firstNumber + +secondNumber);
-var firstNumber2 = Number(prompt("enter first number"));
-var secondNumber2 = Number(prompt("enter second number"));
-alert(firstNumber2 / secondNumber2);
-/script>;
