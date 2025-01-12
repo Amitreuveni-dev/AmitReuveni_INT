d782173629e0c5b0ec493b2de741be3261383d84
amit reuveni <amitr123r@gmail.com>
2025-01-12 17:13:47 +0200

    avg done

--- module-2-js/ts/student_stats.ts
+++ module-2-js/ts/student_stats.ts
@@ -6,3 +6,13 @@
 //     * Stop reading students when user clicks "cancel"
 //     * Display the student's name according to the requested operation
 
+const oparation = prompt("Which oparation you want to perform?\nmax?\nmin? ")
+
+let userName = prompt("Hey what's your name ?")
+
+while (userName === "" || userName === null || !isNaN(userName)){
+    alert("Please enter a valid name")
+    userName = prompt("Hey what's your name ?")
+}alert("Hello " + userName + "!")
+
+let userGrade = prompt("What is your grade please ?")  
