4de0825040b987d432a893436e894f0fd01fdb9a
amit reuveni <amitr123r@gmail.com>
2025-02-09 19:07:30 +0200

    first API first index form

---
+++ module-2-js-ts/ts/08/dist/script.js
@@ -0,0 +1,5 @@
+function invetory(inventory) { }
+function addToInvetory(inventory) { }
+function removeFromInvetory(inventory) { }
+function getEmployeeData(employee) { }
+function manageEmployees(employee) { }
---
+++ module-2-js-ts/ts/08/script.ts
@@ -0,0 +1,23 @@
+// API 
+type inventory = {
+    name: string,
+    id: string,
+    category: string,
+}
+
+type employees = {
+    name: string,
+    department: string,
+    necessaryEquipment: string    
+}
+
+function invetory(inventory: inventory) {}
+
+function addToInvetory(inventory: inventory) {}
+
+function removeFromInvetory(inventory: inventory) {}
+
+function getEmployeeData(employee: employees) {}
+
+function manageEmployees(employee: employees) {}
+
