c672b1d993d2c9b259e42bd5543cd67887c30c47
amit reuveni <amitr123r@gmail.com>
2025-02-18 19:01:50 +0200

    start school project

---
+++ module-2-js-ts/ts/012IDK/SchoolProject.ts
@@ -0,0 +1,19 @@
+type student = {
+    name: string;
+    age: string;
+    id: number;
+    email: string;
+    phoneNumber: string;
+    birthDate: Date;
+    major: string;
+    GPA: number;
+};
+
+type subject = {
+    name: string;
+    description: string;
+    day: string;
+    time: string;
+    professor: string;
+};
+
