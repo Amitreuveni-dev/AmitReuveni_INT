c00178212d6f8a28838d1d48c46fc2dc428b4248
amit reuveni <amitr123r@gmail.com>
2025-03-19 17:41:57 +0200

    add an api server

--- module-3-node/03-promises/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { createServer } from "http";
-
-const server = createServer((req, res) => {
-    console.log(req.method, req.url);
-
-    res.end();
-})
-
-server.listen(8000, () => console.log("Server listening on port 8000"));
